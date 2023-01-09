5312bc0dd394e95f8f9643437d0db753e6f2e5af
Saeed Aldosari <saemaldosari.c@stc.com.sa>
2023-01-09 08:26:50 +0300

    Add Post and delete mapping

--- src/main/java/com/saeedmaldosary/Main.java
+++ src/main/java/com/saeedmaldosary/Main.java
@@ -3,9 +3,7 @@ package com.saeedmaldosary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,4 +31,26 @@ public class Main {
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
     }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {
+
+    }
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
 }
